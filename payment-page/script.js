c3028070cfefe1b25c61828e41b2b3c502c658e1
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-12-05 18:51:03 -0300

    att

--- payment-page/script.js
+++ payment-page/script.js
@@ -1,2 +1,11 @@
-const numberCard = document.getElementById("placeholder-card")
-numberCard.innerText = "0000 0000 0000 0000"
+const numberCard = document.getElementById("placeholder-card");
+numberCard.innerText = "0000000000000000"
+
+const nameCard = document.getElementById("name");
+nameCard.innerText = "Jane Appleseed"
+
+const dateCard = document.getElementById("date");
+dateCard.innerText = "00/00"
+
+const cvcCard = document.getElementById("cvc");
+cvcCard.innerText = "000"
