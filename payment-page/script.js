b3a7c637be106ccece62975f7a507419d2c1706e
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-12-07 15:59:20 -0300

    att

--- payment-page/script.js
+++ payment-page/script.js
@@ -1,13 +1,52 @@
-const form = document.querySelector("form");
+const form = document.querySelector("form"); // Form
+const numberCard = document.getElementById("placeholder-card"); // Number HTML
+const nameCard = document.getElementById("name"); // Name HTML
+const dateCard = document.getElementById("date"); // Date HTML
+const cvcCard = document.getElementById("cvc"); // CVC HTML
+const inputName = document.getElementById("cardholder-name"); // Input Name
+const inputNumberCard = document.getElementById("cardholder-number"); // Input Number
+const inputExpDate = document.getElementById("cardholder-expdate"); // Input Expdate
+const inputMMYY = document.getElementById("cardholder-mmyy"); // Input MMYY
+const inputCVC = document.getElementById("cardholder-cvc"); // Input CVC
+const nameField = document.getElementById("nameField");
+const checkName = document.querySelector("#nameField .bi-check2-circle")
+const errorName = document.querySelector("#nameField .bi-x-circle")
 
-const numberCard = document.getElementById("placeholder-card");
-numberCard.innerText = "0000 0000 0000 0000"
+numberCard.innerText = "0000 0000 0000 0000";
+nameCard.innerText = "Jane Appleseed";
+dateCard.innerText = "00/00";
+//cvcCard.innerText = "000";
 
-const nameCard = document.getElementById("name");
-nameCard.innerText = "Jane Appleseed"
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+})
 
-const dateCard = document.getElementById("date");
-dateCard.innerText = "00/00"
 
-const cvcCard = document.getElementById("cvc");
-cvcCard.innerText = "000"
+inputName.addEventListener("input", () => {
+    const valueName = inputName.value
+    const regexName = /^[a-zA-ZÀ-ÿ\s]+$/ // Regex para permitir apenas letras e valida a string inteira
+    const isValidName = regexName.test(valueName)
+    if (isValidName === true){
+        console.log("Padrão Atendido")
+        errorName.classList.add("d-none")
+        checkName.classList.remove("d-none")
+        inputName.classList.add("form-control-check")
+        inputName.classList.remove("form-control-error")
+        nameField.classList.remove("form-message-error-name")
+        nameField.classList.remove("form-message-error-null")
+    }
+    else{
+        console.log("Padrão Não Atendido")
+        checkName.classList.add("d-none")
+        errorName.classList.remove("d-none")
+        inputName.classList.remove("form-control-check")
+        inputName.classList.add("form-control-error")
+        nameField.classList.add("form-message-error-name")
+        nameField.classList.remove("form-message-error-null")
+        if(valueName === ""){
+            nameField.classList.add("form-message-error-null")
+            inputName.classList.remove("form-control-check")
+        }
+    }
+})
+
